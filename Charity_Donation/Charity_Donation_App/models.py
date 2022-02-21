e1298ccca4401b2120ed322bf786548bbd1f44b5
Emek1902 <autowrapper@gmail.com>
2022-02-21 16:07:05 +0100

    Added models User, Category, Institution, Donation

--- Charity_Donation/Charity_Donation_App/models.py
+++ Charity_Donation/Charity_Donation_App/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=64)
+
+institution_types = ((1, "Fundacja"), (2, "Organizacja pozarządowa"), (3, "Zbiórka lokalna"))
+
+class Institution(models.Model):
+    name = models.CharField(max_length=64)
+    description = models.TextField()
+    type = models.IntegerField(choices=institution_types, default=1)
+    categories = models.ManyToManyField(Category)
+
+class Donation(models.Model):
+    quantity = models.IntegerField()
+    categories = models.ManyToManyField(Category)
+    institution = models.ForeignKey(Institution, on_delete=models.CASCADE)
+    address = models.TextField()
+    phone_number = models.CharField(max_length=9)
+    city = models.CharField(max_length=32)
+    zip_code = models.CharField(max_length=6)
+    pick_up_date = models.DateField()
+    pick_up_time = models.TimeField()
+    pick_up_comment = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
