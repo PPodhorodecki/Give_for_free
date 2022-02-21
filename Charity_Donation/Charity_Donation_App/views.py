ece7743137c5b349d70af3e2dac6179785e304b9
Emek1902 <autowrapper@gmail.com>
2022-02-21 16:56:16 +0100

    Create navigation on htmls

--- Charity_Donation/Charity_Donation_App/views.py
+++ Charity_Donation/Charity_Donation_App/views.py
@@ -1,11 +1,19 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 from django.views import View
+from Charity_Donation_App.models import User, Category, Institution, Donation
 
 
 class LandingPage(View):
     def get(self, request):
-        return render(request, 'index.html')
+        ctx = {}
+        donations = Donation.objects.all()
+        bags = 0
+        for donation in donations:
+            bags += donation.quantity
+        institutions = Institution.objects.count()
+        ctx['bags'] = bags
+        ctx['institutions'] = institutions
+        return render(request, 'index.html', context=ctx)
 
 
 class AddDonation(View):
